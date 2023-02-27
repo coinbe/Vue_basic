bf836fca14c9d5438ac801293ecb80884a7f76a0
coinbe <coin_be@naver.com>
2023-02-27 16:12:36 +0900

    Component Instance

---
+++ src/components/ComponentInstance/Child.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<ul>
+			<li>Component-Instance</li>
+			<button @click="goParent()">VueJS</button>
+		</ul>
+	</div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			test: 'Hi',
+		};
+	},
+	methods: {
+		goParent() {
+			this.$parent.goChild();
+		},
+		hiParent() {
+			alert(this.test);
+		},
+	},
+};
+</script>
+
+<style></style>
---
+++ src/components/ComponentInstance/Praent.vue
@@ -0,0 +1,18 @@
+<template>
+	<div></div>
+</template>
+
+<script>
+export default {
+	data() {
+		return {};
+	},
+	methods: {
+		goChild() {
+			this.$refs.low.hiParent();
+		},
+	},
+};
+</script>
+
+<style></style>
